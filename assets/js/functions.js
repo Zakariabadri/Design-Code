db7b00b4839caa7629bc3768ad19afa9189ad5fc
zakaria <zakaria.badri.contact@gmail.com>
2016-11-25 16:17:07 +0000

    7.2: Mentoring Javascript 1/2

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,24 @@
-//$(function(){});
+$(function(){
+
+  mentoringBubbleClick();
+
+
+});
+
+
+function mentoringBubbleClick(){
+
+  $('.face').on('click',function() {
+
+    var faceTop = $(this).position().top,
+        vertMath = -1 * (faceTop - 230);
+
+    $(this).parent().css('top', vertMath);
+
+    $(this).addClass('has-bubble-open').siblings().removeClass('has-bubble-open');
+  });
+
+}
 
 $(window).scroll(function(){
 
