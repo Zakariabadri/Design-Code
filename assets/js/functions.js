4354d889ff4e24ce47ce0ebb491e12381ec91bec
zakaria <zakaria.badri.contact@gmail.com>
2016-11-29 16:26:12 +0000

    Hour 7.3: Mentoring Responsive 1/2

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -11,10 +11,15 @@ function mentoringBubbleClick(){
   $('.face').on('click',function() {
 
     var faceTop = $(this).position().top,
-        vertMath = -1 * (faceTop - 230);
+        vertMath = -1 * (faceTop - 230),
+        faceLeft = $(this).position().left,
+        horzMath = 0 - faceLeft;
 
-    $(this).parent().css('top', vertMath);
-
+    if($(window).width() > 640){
+      $(this).parent().css('top', vertMath);
+    } else {
+      $(this).parent().css('left', horzMath);
+    }
     $(this).addClass('has-bubble-open').siblings().removeClass('has-bubble-open');
   });
 
@@ -40,13 +45,42 @@ function startMentoring(){
   var wScroll = $(window).scrollTop(),
       sMentoring = $('section.mentoring').offset().top;
 
-  if( sMentoring - 500 < wScroll){
+  if( sMentoring - 500 < wScroll && $(window).width() > 640){
 
     $('.faces').addClass('launched');
-    setTimeout(function(){
-      $('.face:nth-child(3)').addClass('has-bubble-open');
-    }, 400);
-    
+
+    if(!$('.face').hasClass('has-bubble-open')){
+      setTimeout(function(){
+        $('.face:nth-child(3)').addClass('has-bubble-open');
+      }, 400);
+
+    }
+
   }
 
 };
+
+function mentoringNarrowStart(){
+  $('.faces').css({
+    'top': '230px',
+    'left': '0px'
+  });
+  $('.face').first().addClass('has-bubble-open').siblings().removeClass('has-bubble-open');
+}
+
+function mentoringWideStart(){
+  $('.faces').css({
+    'top': '0px',
+    'left': '0px'
+  });
+  $('.face:nth-child(3)').first().addClass('has-bubble-open').siblings().removeClass('has-bubble-open');
+}
+
+
+$(window).resize(function() {
+  if($(window).width() > 640){
+    mentoringWideStart();
+  }else {
+    mentoringNarrowStart();
+  }
+});
